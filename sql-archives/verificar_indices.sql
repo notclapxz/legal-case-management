8f50c4e50f639f32199fece69ab1f7f87a9fa42d
notclapxz <sebastian_zagrev@hotmail.com>
2026-03-03 11:20:22 -0500

    clean: remove AI-related files for portfolio

--- sql-archives/verificar_indices.sql
+++
@@ -1,8 +0,0 @@
--- Verificar índices actuales en todas las tablas
-SELECT 
-    tablename,
-    indexname,
-    indexdef
-FROM pg_indexes
-WHERE schemaname = 'public'
-ORDER BY tablename, indexname;
